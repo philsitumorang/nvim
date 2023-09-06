1f737ebd0894ee8c80d8444f0fe83fc119221d7f
Phil Situmorang <philmsk@gmail.com>
2023-09-06 23:14:17 +0300

    changed barbar's hotkeys to avoid conflicts with tmux bindings

--- mappings/barbar.lua
+++ mappings/barbar.lua
@@ -1,6 +1,6 @@
 local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
+local opts = { noremap = false, silent = true }
 
-map("n", "<C-;>", "<Cmd>BufferPrevious<CR>", opts)
-map("n", "<C-'>", "<Cmd>BufferNext<CR>", opts)
+map("n", "<leader>[", "<Cmd>BufferPrevious<CR>", opts)
+map("n", "<leader>]", "<Cmd>BufferNext<CR>", opts)
 map("n", "<C-x>", "<Cmd>BufferClose<CR>", opts)
