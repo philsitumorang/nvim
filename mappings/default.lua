03a4e4b2cecd763c7950e29529d682e59a74d799
Phil Situmorang <philmsk@gmail.com>
2023-09-06 12:33:44 +0300

    Bind :nohlsearch to ESC button

--- mappings/default.lua
+++ mappings/default.lua
@@ -3,3 +3,6 @@ vim.api.nvim_set_keymap("n", "<C-k>", ":wincmd k<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-j>", ":wincmd j<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-h>", ":wincmd h<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-l>", ":wincmd l<CR>", { noremap = true })
+
+-- Stop searching
+vim.api.nvim_set_keymap("n", "<Esc>", ":nohlsearch<CR>", { silent = true })
