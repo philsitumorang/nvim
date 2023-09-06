55d5c697c9b6c344f17caef82fdd2b395694dc04
Phil Situmorang <philmsk@gmail.com>
2023-09-06 23:19:00 +0300

    barbar - better way to switch prev/next tabs (ShiftTab/Tab)

--- mappings/barbar.lua
+++ mappings/barbar.lua
@@ -1,6 +1,6 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = false, silent = true }
 
-map("n", "<leader>[", "<Cmd>BufferPrevious<CR>", opts)
-map("n", "<leader>]", "<Cmd>BufferNext<CR>", opts)
+map("n", "<S-Tab>", "<Cmd>BufferPrevious<CR>", opts)
+map("n", "<Tab>", "<Cmd>BufferNext<CR>", opts)
 map("n", "<C-x>", "<Cmd>BufferClose<CR>", opts)
