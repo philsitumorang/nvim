8e9af72b57826495d9f56d1ad83bbc84feb8333e
Phil Situmorang <philmsk@gmail.com>
2025-03-31 16:52:01 +0100

    fix: remove not requied lsp servers

--- configs/lsp.lua
+++ configs/lsp.lua
@@ -1,24 +1,5 @@
 local mason = require("mason")
 local lspconfig = require("mason-lspconfig")
 
-local snippetSupport = {
-	capabilities = {
-		textDocument = {
-			completion = {
-				completionItem = {
-					snippetSupport = true,
-				},
-			},
-		},
-	},
-}
-
 mason.setup({})
-lspconfig.setup({
-	ensure_installed = {
-		"tailwindcss",
-		"ts_ls"
-	},
-})
-require("lspconfig").tailwindcss.setup({})
-require("lspconfig").ts_ls.setup(snippetSupport)
+lspconfig.setup({})
