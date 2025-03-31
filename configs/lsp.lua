146af0d90bf8e3e26e366e6050a40b374bd3f261
Phil Situmorang <philmsk@gmail.com>
2025-03-31 17:09:01 +0100

    fix: automatically install lsp with the default config

--- configs/lsp.lua
+++ configs/lsp.lua
@@ -2,4 +2,11 @@ local mason = require("mason")
 local lspconfig = require("mason-lspconfig")
 
 mason.setup({})
-lspconfig.setup({})
+lspconfig.setup({
+  automatic_installation = true,
+  handlers = {
+    function(server_name)
+      require("lspconfig")[server_name].setup({})
+    end,
+  },
+})
