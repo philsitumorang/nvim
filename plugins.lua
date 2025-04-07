4ffc15c8e783ff56f51da5517f52babb383d58cb
Phil Situmorang <philmsk@gmail.com>
2025-04-07 17:13:04 +0100

    chore: add moonfly theme

--- plugins.lua
+++ plugins.lua
@@ -71,12 +71,13 @@ return {
       vim.keymap.set("n", "<leader>tt", "<cmd>ToggleTerm<CR>", { desc = "Terminal" })
     end,
   },
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.cmd([[colorscheme tokyonight-storm]])
-		end,
-	},
+  { 
+    "bluz71/vim-moonfly-colors", 
+    name = "moonfly", 
+    lazy = false, 
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme moonfly]])
+    end,
+  },
 }
